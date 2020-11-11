91b6642bf21be506e2efa87dd967a1dd454cc18c
unknown <louis-blau@orange.fr>
2020-11-11 12:26:30 +0100

    slight modification

--- TreeApp/main.cpp
+++ TreeApp/main.cpp
@@ -3,11 +3,11 @@
 #include <QApplication>
 #include <QLabel>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
     QLabel label ("A",&w);
+    label.setGeometry(100,100,10,10);
     w.show();
     return a.exec();
 }
